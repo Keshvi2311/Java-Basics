5de12e526fe5c505306a3ea0bbf92654612e3713
Keshvi Patel <keshvipatel2311@gmail.com>
2026-02-24 21:34:17 +0530

    Initial Java basics project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum is: " + sum);
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
