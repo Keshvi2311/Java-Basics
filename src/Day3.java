6b6a2b0c8c3a7d85f16a581598bdb083570dec9b
Keshvi Patel <keshvipatel2311@gmail.com>
2026-02-25 23:08:33 +0530

    Day 3 Done.

---
+++ src/Day3.java
@@ -0,0 +1,45 @@
+public class Day3 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        int result = calculateSum(x, y);
+
+        System.out.println("x = " + x);
+        System.out.println("result = " + result);
+
+        int[] a = {4,9,10,999,56,23,6};
+        int sum = 0;
+        int max = 0;
+        int count = 0;
+
+        for (int i = 0 ; i<a.length ; i++){
+            System.out.println(a[i]);
+            sum = a[i] + sum;
+            if (a[i]>max){
+                max = a[i];
+            }
+            else{
+                max = max;
+            }
+            if (a[i] % 2 == 0){
+                count ++;
+            }
+        }
+        System.out.println("Count  = " +count);
+
+        for (int i = a.length-1 ; i>=0 ; i--){
+            System.out.println(a[i]);
+        }
+
+        System.out.println(sum);
+        System.out.println(max);
+
+
+    }
+
+    public static int calculateSum(int a, int b) {
+        a = 100;
+        return a + b;
+    }
+}
